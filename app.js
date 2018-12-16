5881999d0787b237c78fa5624a96e078df9ee7d7
Shomari Pope <shomaripope@gmail.com>
2018-12-16 12:37:51 -0500

    created logic to spit strings to an array

--- app.js
+++ app.js
@@ -26,4 +26,21 @@ let readfile = function () {
        $("#playerImage").fadeOut(120000);
    })
 
-   
+   var file = "/Users/shomaripope/projects/unit1Project/usa.txt";
+   function getFile(){
+       $.get(file,function(txt){
+           var lines = txt.responseText.split("\n");
+           for (var i = 0, len = lines.length; i < len; i++) {
+               save(lines[i]);
+           }
+       }); 
+   }
+splitWord = [];
+guessWord = "dynamic";
+for ( let i = 0; i < guessWord.length; i ++){
+    splitWord.push(guessWord.charAt(i));
+    console.log(guessWord.charAt(i));
+    console.log(splitWord)
+}
+
+
